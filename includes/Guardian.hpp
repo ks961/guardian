7fcc6658bfb7651b9ab2a77b8d8181f3d14082d1
ks961 <ksudhanshu961@gmail.com>
2023-12-25 16:19:37 +0530

    fixes

--- includes/Guardian.hpp
+++ includes/Guardian.hpp
@@ -9,6 +9,9 @@ including without limitation the rights to use, copy, modify, merge, publish, di
 and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
 subject to the following conditions:
 
+1. Proper attribution to the original author's GitHub account must be provided in a professional manner.
+This attribution should prominently display the GitHub account username associated with the original work.
+
 The above copyright notice and this permission notice shall be included in all copies or substantial
 portions of the Software.
 
